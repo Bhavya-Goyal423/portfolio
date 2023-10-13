b3285850d8d6a9c19fe5fb13c0ba1cc17a4c0c49
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-13 18:39:52 +0530

    added more projects to project section

--- src/data.js
+++ src/data.js
@@ -1,23 +1,24 @@
 const data = [
   {
-    href: "https://card-flip-ruddy.vercel.app/",
-    name: "Card-Flip Game",
-    src: "/project/cardflip.png",
+    href: "https://omnifood-liart.vercel.app/",
+    name: "Omnifood Delivery",
+    src: "/project/omnifood.png",
   },
   {
     href: "https://card-flip-ruddy.vercel.app/",
     name: "Card-Flip Game",
     src: "/project/cardflip.png",
   },
+
   {
-    href: "https://card-flip-ruddy.vercel.app/",
-    name: "Card-Flip Game",
-    src: "/project/cardflip.png",
+    href: "https://currency-converter-blush-nu.vercel.app/",
+    name: "Currency Converter",
+    src: "/project/currency.png",
   },
   {
     href: "https://card-flip-ruddy.vercel.app/",
-    name: "Card-Flip Game",
-    src: "/project/cardflip.png",
+    name: "Job Recruiter Node JS",
+    src: "/project/job.png",
   },
 ];
 
