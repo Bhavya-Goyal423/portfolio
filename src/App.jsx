37a0dc9968ecc6abdf37b275a0c98c2a0080849a
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-13 08:35:31 +0530

    added media queries

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,17 @@ function App() {
         </div>
         <div className="details">
           <div className="my-details">
-            <img src="/img/me2.jpg" alt="me" />
+            <img src="/img/me.jpg" alt="me" />
             <p className="name">Bhavya Goyal</p>
             <p className="role">Web Developer</p>
           </div>
           <div className="contact-social">
             {/* !------------Instagram----------------  */}
-            <a href="https://www.instagram.com/bhavyagoel1422/" target="_blank">
+            <a
+              href="https://www.instagram.com/bhavyagoel1422/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="1em"
@@ -113,7 +117,11 @@ function App() {
                 </svg>
               </span>
             </a>
-            <a href="https://wa.me/+919599639538" target="_blank">
+            <a
+              href="https://wa.me/+919599639538"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="32"
@@ -124,6 +132,19 @@ function App() {
                 <path
                   fill="#888888"
                   d="M19.05 4.91A9.816 9.816 0 0 0 12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01zm-7.01 15.24c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.264 8.264 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c2.2 0 4.27.86 5.82 2.42a8.183 8.183 0 0 1 2.41 5.83c.02 4.54-3.68 8.23-8.22 8.23zm4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.78.97c-.14.17-.29.19-.54.06c-.25-.12-1.05-.39-1.99-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43s.17-.25.25-.41c.08-.17.04-.31-.02-.43s-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.43.06-.66.31c-.22.25-.86.85-.86 2.07c0 1.22.89 2.4 1.01 2.56c.12.17 1.75 2.67 4.23 3.74c.59.26 1.05.41 1.41.52c.59.19 1.13.16 1.56.1c.48-.07 1.47-.6 1.67-1.18c.21-.58.21-1.07.14-1.18s-.22-.16-.47-.28z"
+                />
+              </svg>
+            </a>
+            <a href="mailto:goel2000bhavya@gmail.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#888888"
+                  d="m20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z"
                 />
               </svg>
             </a>
